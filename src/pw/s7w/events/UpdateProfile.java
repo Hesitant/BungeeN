1a9d545ba05dd8f8ec53a4dc7e6c8f51471c5e7d
TheS7W <loadingpackets@gmail.com>
2015-05-16 22:46:45 +0100

    Adding creating profiles.

--- src/pw/s7w/events/UpdateProfile.java
+++ src/pw/s7w/events/UpdateProfile.java
@@ -6,8 +6,8 @@ import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import pw.s7w.Main;
+import pw.s7w.objects.Profile;
 
-import java.awt.*;
 
 /**
  * Created by S7W on 16/05/2015.
@@ -18,9 +18,11 @@ public class UpdateProfile implements Listener {
     @EventHandler
     public void onHandleLogin(LoginEvent e) {
         ProxiedPlayer p = (ProxiedPlayer) e.getConnection();
+        Profile profile = new Profile(p, p.getAddress().toString(), null, null);
+        
         for(ProxiedPlayer players : ProxyServer.getInstance().getPlayers()) {
             if(players.hasPermission("network.join")) {
-                players.sendMessage(p.getName() + "§c has joined network. Server [" + Main.get().getProxy().getName() + "]");
+                players.sendMessage(p.getName() + "§c has joined the network. Server [" + Main.get().getProxy().getName() + "]");
             }
         }
     }
